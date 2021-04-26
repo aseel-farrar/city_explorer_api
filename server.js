27aafb21fa80a239be79d69ab1df282dcded5fcd
aseel-farrar <aseelfarrar1@hotmail.com>
2021-04-26 03:48:14 +0300

    location route

--- server.js
+++ server.js
@@ -8,7 +8,39 @@ require('dotenv');
 const server = express();
 server.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+//>>>>>>>>>>>>>>>>>ROUTES<<<<<<<<<<<<<<<<<<<
+
+///home route
+server.get('/', (req, res) => {
+  res.send('Home Page...');
+});
+
+///location route
+server.get('/location', locationHandel);
+
+//ERROR page...
+server.get('*', (req, res) => {
+  res.send('ERROR page...');
+});
+//>>>>>>>>>>>>>>>>>ROUTES<<<<<<<<<<<<<<<<<<<
+
+
+
+
+///location route handler
+function locationHandel(req, res) {
+  const geoData = require('./data/location.json');
+  let locationData = new Location(geoData);
+  res.send(locationData);
+}
+//location constructor
+function Location(geoData) {
+  this.search_query = 'Lynwood';
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
+}
 
 
 
@@ -17,6 +49,7 @@ const PORT = process.env.PORT || 5000;
 
 
 
-server.listen(PORT,()=>{
+
+server.listen(PORT, () => {
   console.log(`listening to PORT ${PORT}`);
 });
