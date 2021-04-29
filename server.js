6d9858ef00b93eac05c4bd8367ebf062215f875e
aseel-farrar <aseelfarrar1@hotmail.com>
2021-04-29 08:38:42 +0300

    lab08

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 'use strict';
 
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
 const superagent = require('superagent');
-// const { connected } = require('node:process');
-require('dotenv').config();
+const pg = require('pg');
 
 
 const server = express();
 const PORT = process.env.PORT || 4000;
 server.use(cors());
+
+const client = new pg.Client(process.env.DATABASE_URL);
+// const client = new pg.Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: { rejectUnauthorized: false }
+// });
 
 //>>>>>>>>>>>>>>>>> ROUTES <<<<<<<<<<<<<<<<<<<
 
@@ -26,7 +32,7 @@ server.get('*', errorHandle);
 
 //>>>>>>>>>>>>>>>>> ROUTES <<<<<<<<<<<<<<<<<<<
 
-// parks route handler
+//!  parks route handler
 function parksHandle(req, res) {
   let parkName = req.query.parkcode;
   const key = process.env.PARKS_API_KEY;
@@ -45,7 +51,7 @@ function parksHandle(req, res) {
     });
 }
 
-///location route handler
+//!  weather route handler
 function weatherHandle(req, res) {
   let results = [];
   let cityName = req.query.city;
@@ -59,6 +65,40 @@ function weatherHandle(req, res) {
         return (weather);
       });
       res.send(results);
+    });
+}
+
+//!  location route handler
+function locationHandel(req, res) {
+  let cityName = req.query.city;
+  //GET DATA FROM DATABASE
+  let SQL = `SELECT * FROM locations WHERE search_query = '${cityName}';`;
+  client.query(SQL)
+    .then(result => {
+      if (result.rows.length === 0) {// RETURN FROM API SERVER
+        const key = process.env.GEOCODE_API_KEY;
+        //https://eu1.locationiq.com/v1/search.php?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json
+        let LocationURL = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
+
+        superagent.get(LocationURL)
+          .then(geoData => {
+            let gData = geoData.body;
+            const locationData = new Location(cityName, gData);
+            // INSERT DATA TO DATABASE
+            let SQL = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) RETURNING *;`;
+            let safeValues = [locationData.search_query, locationData.formatted_query, locationData.latitude, locationData.longitude];
+            client.query(SQL, safeValues)
+              .then(result => {
+                res.send(result.rows[0]);
+              });
+          });
+      }
+      else { // RETURN FROM DATABASE
+        res.send(result.rows[0]);
+      }
+    })
+    .catch(error => {
+      res.send(error);
     });
 }
 
@@ -77,20 +117,6 @@ function Weather(weatherData) {
   this.time = new Date(weatherData.datetime).toString().slice(0, 15);// >>>>> data.datetime <<<<<<<
 }
 
-///location route handler
-function locationHandel(req, res) {
-  let cityName = req.query.city;
-  const key = process.env.GEOCODE_API_KEY;
-  //https://eu1.locationiq.com/v1/search.php?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json
-  let LocationURL = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
-
-  superagent.get(LocationURL)
-    .then(geoData => {
-      let gData = geoData.body;
-      const locationData = new Location(cityName, gData);
-      res.send(locationData);
-    });
-}
 
 //location constructor
 function Location(cityName, geoData) {
@@ -108,7 +134,10 @@ function errorHandle(req, res) {
 }
 
 // add listener
-server.listen(PORT, () => {
-  console.log(`listening to PORT ${PORT}`);
-  console.log(`coding...`);
-});
+client.connect()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`listening to PORT ${PORT}`);
+      console.log(`coding...`);
+    });
+  });
